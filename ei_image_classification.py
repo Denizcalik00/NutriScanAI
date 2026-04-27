c7dca4e0852b3e7d19c6b3f764ef0b2810c4a060
ritz08-ul <25236164@studentmail.ul.ie>
2026-04-27 15:09:17 +0100

    Update ei_image_classification.py

--- ei_image_classification.py
+++ ei_image_classification.py
@@ -1,41 +1,55 @@
-# Edge Impulse - OpenMV Image Classification Example
-#
-# This work is licensed under the MIT license.
-# Copyright (c) 2013-2024 OpenMV LLC. All rights reserved.
-# https://github.com/openmv/openmv/blob/master/LICENSE
+import sensor, time, ml, gc, uos
+import socket
+import json
 
-import sensor, time, ml, uos, gc
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.set_windowing((240, 240))
+sensor.skip_frames(time=2000)
 
-sensor.reset()                         # Reset and initialize the sensor.
-sensor.set_pixformat(sensor.RGB565)    # Set pixel format to RGB565 (or GRAYSCALE)
-sensor.set_framesize(sensor.QVGA)      # Set frame size to QVGA (320x240)
-sensor.set_windowing((240, 240))       # Set 240x240 window.
-sensor.skip_frames(time=2000)          # Let the camera adjust.
+# Server config (your laptop IP)
+SERVER_IP = "192.168.1.5"
+SERVER_PORT = 8000
 
-net = None
-labels = None
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((SERVER_IP, SERVER_PORT))
 
-try:
-    # load the model, alloc the model file on the heap if we have at least 64K free after loading
-    net = ml.Model("trained.tflite", load_to_fb=uos.stat('trained.tflite')[6] > (gc.mem_free() - (64*1024)))
-except Exception as e:
-    print(e)
-    raise Exception('Failed to load "trained.tflite", did you copy the .tflite and labels.txt file onto the mass-storage device? (' + str(e) + ')')
+nutrition = {
+    "Fries": ["312 kcal", "3.4g protein", "41g carbs"],
+    "Burger": ["295 kcal", "17g protein", "30g carbs"],
+    "Pizza": ["266 kcal", "11g protein", "33g carbs"],
+    "Banana": ["89 kcal", "1.1g protein", "23g carbs"],
+    "Broccoli": ["55 kcal", "3.7g protein", "11g carbs"]
+}
 
-try:
-    labels = [line.rstrip('\n') for line in open("labels.txt")]
-except Exception as e:
-    raise Exception('Failed to load "labels.txt", did you copy the .tflite and labels.txt file onto the mass-storage device? (' + str(e) + ')')
+net = ml.Model("trained.tflite", load_to_fb=uos.stat('trained.tflite')[6] > (gc.mem_free() - (64*1024)))
+labels = [line.strip() for line in open("labels.txt")]
 
 clock = time.clock()
-while(True):
-    clock.tick()
+THRESHOLD = 0.80
 
+while True:
+    clock.tick()
     img = sensor.snapshot()
 
-    predictions_list = list(zip(labels, net.predict([img])[0].flatten().tolist()))
+    scores = net.predict([img])[0].flatten().tolist()
+    predictions = list(zip(labels, scores))
 
-    for i in range(len(predictions_list)):
-        print("%s = %f" % (predictions_list[i][0], predictions_list[i][1]))
+    best_label, best_score = max(predictions, key=lambda x: x[1])
 
-    print(clock.fps(), "fps")
+    if best_score > THRESHOLD:
+        payload = {
+            "label": best_label,
+            "score": float(best_score),
+            "nutrition": nutrition.get(best_label, [])
+        }
+    else:
+        payload = {
+            "label": "Unknown",
+            "score": float(best_score)
+        }
+
+    client.send((json.dumps(payload) + "\n").encode())
+
+    img.draw_string(10, 10, best_label, color=(255, 0, 0), scale=2)
